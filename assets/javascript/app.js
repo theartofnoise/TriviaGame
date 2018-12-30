765724a764e267bd98a5da1e1f20e593bbd4b06c
Art G <theartofnoise@gmail.com>
2018-12-30 11:10:01 -0500

    much more, 2nd run

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,41 @@
 
 
 $(document).ready (function () {
-    
-    // hides start
+    var totalQuestions = 5;
     var correctAnswers = 0;
     var wrongAnswers = 0;
+    var unansweredQuestions = 0;
+    var answered = 0;
     gameStart();
     
+    // hides results, main and shows start
     function gameStart(){
         $("#startButton").show();
+
+        $("#timer").hide();
         $("#main").hide();
         $("#results").hide();
+        var ele = document.getElementsByName("input");
+        for(var i=0;i<ele.length;i++)
+        ele[i].checked = false;
         
     };
     
-    //starts trivia
+    //starts trivia shows main, starts timer
     $("#startButton").on("click", function(){
         $("#startButton").hide();
         $("#main").show();
+        $("#timer").show();
         //starts timers
         startTimer();
 
     });
     // how much time and timer
-    
     function startTimer(){
         var time = 15;
         $("#timer").text("Time Left: "+time);
         var timer = setInterval(function(){
             time--;
-            console.log(time);
             $("#timer").text("Time Left: "+time);
             if (time===0)
             {
@@ -38,7 +44,20 @@ $(document).ready (function () {
                 done();
             }
         }, 1000);
-        
+
+        //radio checker
+        $('input[type="radio"]').change(function() {
+            if($(this).val()=="correct"){
+                correctAnswers++;
+                answered++;
+                console.log("right "+correctAnswers);
+            } else {
+                wrongAnswers++;
+                answered++;
+                console.log("wrong "+wrongAnswers);
+            }
+
+        });
         
         //done button
         $("#done").on("click", function(){
@@ -47,23 +66,21 @@ $(document).ready (function () {
         });
         //done function
         function done() {
-            clearInterval(timer);
-            var radioValue = $("input[data-type='correct']:checked").val();
-    
-            if(radioValue){
-                correctAnswers++;
-                console.log("right "+correctAnswers);
-            } else {
-                wrongAnswers++;
-                console.log("wrong "+wrongAnswers);
-            }
-            
+            clearInterval(timer);           
             $("#main").hide();
             $("#results").show();
+            $("#rightAnswers").text(correctAnswers);
+            $("#wrongAnswers").text(wrongAnswers);
+            $("#unansweredQuestions").text(totalQuestions-answered);
 
 
         };
         $("#tryAgain").on("click", function(){
+            //clear radios ****************
+            $('input[type="radio"]').attr('checked',false);
+            correctAnswers = 0;
+            wrongAnswers = 0;
+            answered = 0;
             gameStart();
         })
     };
